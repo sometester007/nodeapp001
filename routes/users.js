1fc23c51a1612a9e642349c376025339e03d6c2c
sometester007 <gtinjava@gmail.com>
2018-03-21 17:09:25 -0700

    init

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('users list');
+});
+router.get('/:id', function(req, res, next) {
+  res.send(req.params.id);
 });
 
 module.exports = router;
