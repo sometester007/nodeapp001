9e4fd89ce577065da717a5f00fbaa596c326665e
sometester007 <gtinjava@gmail.com>
2018-03-21 18:05:34 -0700

    test

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('users list');
+  // res.send('users list');
+  let users=[{name:"logan",age:12},{name:"logan",age:22}];
+  res.json(users);
 });
 router.get('/:id', (req, res, next)=> {
-  res.send(req.params.id); 
+  res.send(req.params.id);
 });
 
 module.exports = router;
